63e244baf4cd1a74e598b62789340e845b4c87ae
Sasha Fedorova <sasha@ece.ubc.ca>
2016-01-13 16:02:22 -0800

    Added an optinal string to specify the pattern to search for the performance numbers in the data file

--- tools/misc/get_ops_teststat.py
+++ tools/misc/get_ops_teststat.py
@@ -5,7 +5,7 @@ import argparse
 import re
 
 
-def parse_file(fname):
+def parse_file(fname, pattern):
 
     try:
         fd = open(fname, "r");
@@ -20,7 +20,8 @@ def parse_file(fname):
     p_num = re.compile('[0-9]+');
 
     # Regex for the number of ops
-    p_populate = re.compile('stat:[0-9]+');
+    print('[0-9]+ '+ pattern);
+    p_populate = re.compile('[0-9]+ '+ pattern);
 
     for line in fd:
         match = p_threadstring.search(line);
@@ -43,12 +44,19 @@ def main():
                                      'test.stat file.');
     parser.add_argument('files', type=str, nargs='+',
                         help='grep output files to process');
+    parser.add_argument("-p", "--pattern", type=str, nargs=1,
+                        help="string following the performance number, as"
+                        "in \"500 ops\". \"ops\" is the string we will search "
+                        "for to extract performance numbers.");
 
     args = parser.parse_args();
-    print args.files;
+    print("The files are: ");
+    print args.files
+    print("Pattern string is: "),
+    print args.pattern;
 
     for fname in args.files:
-        parse_file(fname);
+        parse_file(fname, args.pattern[0]);
 
 if __name__ == '__main__':
     main()
