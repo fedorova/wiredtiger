892a502b38b65d9896d683920c12fe6496741633
Sasha Fedorova <sasha@ece.ubc.ca>
2016-05-03 13:16:32 -0700

    Fix compilation errors with timing enabled

--- src/os_win/os_sleep.c
+++ src/os_win/os_sleep.c
@@ -13,9 +13,10 @@
  *	Pause the thread of control.
  */
 void
-__wt_sleep(uint64_t seconds, uint64_t micro_seconds)
+__wt_sleep(WT_SESSION_IMPL *session, uint64_t seconds, uint64_t micro_seconds)
 {
 	DWORD dwMilliseconds;
+	WT_UNUSED(session);
 
 	/*
 	 * If the caller wants a small pause, set to our
